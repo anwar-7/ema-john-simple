f34626ee63a3175fd936af437650d562ba640fe0
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-03-22 20:11:18 +0600

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <h1>This is App.js</h1>
+      <Shop></Shop>
     </div>
   );
 }
