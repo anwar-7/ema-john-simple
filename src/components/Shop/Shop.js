f990eb17fe7ef832f027afd37c7575317e71b343
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-03-30 01:17:26 +0600

    update 49th module

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,57 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
-  // console.log([products, setProducts]);
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
-  const [cart, setCart] = useState([]);
-  // console.log([cart, setCart]);
-  /* 
-  //1. What is useState()?
-  //2. when and why we declare it?
-  //3. what function/happened inside useState()?
-  */
-  useEffect(() => {
-    fetch('products.json')
-      .then((res) => res.json())
-      .then((json) => setProducts(json));
-  }, []);
+    useEffect( () =>{
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data =>setProducts(data))
+    }, []);
 
-  const handleAddToCart = (product) => {
-    /* 
-    //4. why we set or write parameter name as (product)?
-    //5. what is going on here? 
-    //6. what comes here and where it is gone?
-    //7. previously what is in the cart? why we do this [...cart, product]? 
-    after doing that what happened in the setCart() and its useState()?
-    */
-    // const newCart = [...cart, product];
-    // setCart(newCart);
-    setCart([...cart, product]);
-    // console.log([...cart, product]);
-    addToDb(product.id);
-  };
+    useEffect( () =>{
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products])
 
-  return (
-    <div className="shop-container">
-      <div className="products-container">
-        {products.map((product) => (
-          <Product
-            key={product.id}
-            product={product}
-            handleAddToCart={handleAddToCart}
-          ></Product>
-        ))}
-      </div>
-      <div className="cart-container">
-        <Cart cart={cart}></Cart>
-      </div>
-    </div>
-  );
+    const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        
+        setCart(newCart);
+        addToDb(selectedProduct.id);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product=><Product 
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                        ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
 };
 
 export default Shop;
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,87 +1,54 @@
-/* 
-
 // use local storage to manage cart data
-const addToDb = (id) => {
-  let shoppingCart = {};
+const addToDb = id =>{
+    let shoppingCart = {};
 
-  //get the shopping cart from local storage
-  const storedCart = localStorage.getItem('shopping-cart');
-  if (storedCart) {
-    shoppingCart = JSON.parse(storedCart);
-  }
-
-  // add quantity
-  const quantity = shoppingCart[id];
-  if (quantity) {
-    const newQuantity = quantity + 1;
-    shoppingCart[id] = newQuantity;
-  } else {
-    shoppingCart[id] = 1;
-  }
-  localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-};
-
-const removeFromDb = (id) => {
-  const storedCart = localStorage.getItem('shopping-cart');
-  if (storedCart) {
-    const shoppingCart = JSON.parse(storedCart);
-    if (id in shoppingCart) {
-      delete shoppingCart[id];
-      localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
     }
-  }
-};
 
-const deleteShoppingCart = () => {
-  localStorage.removeItem('shopping-cart');
-};
+    // add quantity
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+}
 
-export { addToDb, removeFromDb, deleteShoppingCart };
+const getStoredCart = () =>{
+    let shoppingCart = {};
 
-*/
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+    return shoppingCart;
+}
 
-/*
- 
-const addToDb = (id) => {
-  let shoppingCart = {};
+const removeFromDb = id =>{
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        const shoppingCart = JSON.parse(storedCart);
+        if(id in shoppingCart){
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+        }
+    }
+}
 
-  //get the shopping cart from local storage
-  const storedCart = localStorage.getItem('shopping-cart');
+const deleteShoppingCart = () =>{
+    localStorage.removeItem('shopping-cart');
+}
 
-  if (storedCart) {
-    console.log('storedCart has a or some value (1)');
-    shoppingCart = JSON.parse(storedCart);
-  }
-
-  //add quantity
-  const quantity = shoppingCart[id]; // what is inside of the quantity?
-  if (quantity) {
-    console.log('find quantity and increase the value (2)');
-    const newQuantity = quantity + 1;
-    shoppingCart[id] = newQuantity;
-    // localStorage.setItem(id, newQuantity);
-  } else {
-    console.log('new value add to the shoppingCare (3)');
-    shoppingCart[id] = 1; // why we set 1?
-  }
-  localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-  // why bring it here from the top?
-  // please describe the hole thing happened in that line and why we put in here?
-};
-
-export { addToDb }; 
-
-*/
-
-const addToDb = (id) => {
-  console.log(id);
-  const quantity = localStorage.getItem(id);
-  if (quantity) {
-    const newQuantity = quantity + 1;
-    localStorage.setItem(id, newQuantity);
-  } else {
-    localStorage.setItem(id, 1);
-  }
-};
-
-export { addToDb };
+export {
+    addToDb, 
+    getStoredCart,
+    removeFromDb,
+    deleteShoppingCart
+}
