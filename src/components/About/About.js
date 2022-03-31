cb3d60779764c7a51cc77387b619138edc2aa33a
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-04-01 03:03:06 +0600

    make our own hooks

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h3>we are gopone achi...</h3>
+      <p>
+        <small>you can't know about us...</small>
+      </p>
+    </div>
+  );
+};
+
+export default About;
