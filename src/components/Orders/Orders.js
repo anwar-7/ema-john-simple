fb3543eb4a87a968b7bddcb93c639d2b4a0c7f6c
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-04-02 15:51:41 +0600

    delete button update

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useCart } from '../../hooks/useCart';
 import { useProducts } from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
   const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
+
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+  };
   return (
     <div>
-      <h3>This is orders...!: {products.length}</h3>
+      <div className="shop-container">
+        <div className="review-item-container">
+          {cart.map((product) => (
+            <ReviewItem
+              key={product.id}
+              product={product}
+              handleRemoveProduct={handleRemoveProduct}
+            />
+          ))}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart}></Cart>
+        </div>
+      </div>
     </div>
   );
 };
